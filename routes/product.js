7374cc8e26fb3cf41ab990a555b54211691aca8b
deijaparker <deijaparker1@outlook.com>
2024-09-09 18:19:15 -0700

    Set up routes for product CRUD operations

---
+++ routes/product.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/Product");
+
+// Create Product
+router.post("/", async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get All Products
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
